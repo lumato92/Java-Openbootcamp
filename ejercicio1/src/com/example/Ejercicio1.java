5cd288803c933363606db31673addb9ec0c4ec01
lumato92 <55516181+lumato92@users.noreply.github.com>
2022-09-09 21:46:55 -0300

    ejercicio1

---
+++ ejercicio1/src/com/example/Ejercicio1.java
@@ -0,0 +1,32 @@
+package com.example;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        //Variables
+
+        //Numericas
+        byte variable0 = 22;
+        System.out.println(variable0);
+        int variable1 = 1;
+        System.out.println(variable1);
+        float variable2 = 2.4f;
+        System.out.println(variable2);
+        double variable3 = 3.56d;
+        System.out.println(variable3);
+        long variable4 = 3243;
+        System.out.println(variable4);
+
+        //Texto
+
+        char variable5 = 'g';
+        System.out.println(variable5);
+        String variable6 = "jaja";
+        System.out.println(variable6);
+
+        //Logicos
+
+        boolean variable7 = true;
+        System.out.println(variable7);
+    }
+}
